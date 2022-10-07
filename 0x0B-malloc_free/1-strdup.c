42285c66ea1bc31af8ec09e4119ac253a2be2ff8
Amhayes Tadios <yeamhagroup@gmail.com>
2022-10-07 11:33:56 -0700

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _strdup - returns a pointer to a string copy
+ * @str: string to be copied
+ *
+ * Return: pointer to string or null if str is null
+ */
+
+char *_strdup(char *str)
+{
+	char *strc;
+
+	int i = 0;
+	int len = 0;
+
+	while (str[len])
+	{
+		len++;
+	}
+
+	len++;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	strc = (char *)malloc(sizeof(char) * len);
+
+	if (strc == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[i])
+	{
+		strc[i] = str[i];
+		i++;
+	}
+
+	return (strc);
+}
